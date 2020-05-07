2c96c2ee7d5b4e0f73564afd4e16a09a8580fba8
Joyce <nokjo86@hotmail.com>
2020-05-07 21:27:43 +1000

    added controllers

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,52 @@
+class LocationsController < ApplicationController
+before_action :authenticate_user!, only: [:create, :destroy]
+before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @location = Location.all
+  end
+
+  def show
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = location.new(location_params)
+    @location.user = current_user
+    if @location.save
+      redirect_to @location
+    else
+      render :new
+  end
+
+  def edit
+  end
+  
+  def update
+    if @location.update(location_params)
+      redirect_to @location
+    else
+      render :edit
+    end   
+  end
+  
+  def destroy
+    @location.destroy
+    redirect_to locations_url
+  end
+
+  private
+  
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :description)
+  end
+
+end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
   has_many :users_locations
   has_many :users, through: :users_locations
+  has_one_attached :image
 end
