7f31e886d6eb767fba9aafd58b47105a0a02ce92
Joyce <nokjo86@hotmail.com>
2020-05-07 20:41:26 +1000

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,69 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+puts "Seeded data successfully deleted"
+puts "Seeding Users"
+users = [{
+    name: 'admin',
+    email: 'admin@gmail.com(opens in new tab)',
+    password: 'password'
+  },  
+  {
+    name: 'user_1',
+    email: 'user_1@gmail.com(opens in new tab)',
+    password: 'password'
+  },
+  {
+    name: 'user_2',
+    email: 'user_2@gmail.com(opens in new tab)',
+    password: 'password'
+  }
+]
+users.each do |user|
+  User.create(
+    name: user[:name],
+    email: user[:email],
+    password: user[:password]
+  )
+end
+puts "Users successfully seeded"
+# Gear 
+puts "Seeding Gears"
+gears = ['water bottle', 'sleeping bag', 'compass', 'mat', 'travel pillow', 'backpack', 'tent', 'swiss army knife', 'water treatment tablets', 'watch', 'raincover', 'walking poles']
+gears.each do |gear|
+  Gear.create(
+    name: gear
+  )
+end
+puts "Gears successfully seeded"
+# location name , description, ** latitude and longitude to be added
+puts "Seeding Locations"
+locations = [
+  {
+    name: 'The Pinnacle, Victoria, AU',
+    description: 'Bring a compass.  It\'s awkward when you have to eat your friends',
+    # latitude: 
+    # longitude: 
+  },
+  {
+    name: 'Larapinta Trail, Northern Territory, AU',
+    description: 'The Larapinta Trail is a challenging 13 to 15 day trek that covers 223km of beautiful scenery. It's definitely not for beginners, and it's best to walk during the cooler months from April to September. You'll need rest days and food drops to complete the hike, and while it's hard, it is one-way with relatively good access at both entry points. For hikers willing to take on the challenge, the reward will be unique and stunning scenery.',
+    # latitude: 
+    # longitude: 
+  },
+  {
+    name: 'The Arkaba Walk, South Australia, AU',
+    description: 'This walk traverses South Australia's ancient, iconic Flinders Ranges and is the perfect hike to explore this unique part of the country. This experience will have you seeing native wildlife like kangaroos, wallaroos and emus, as well as rich and spectacular scenery.',
+    # latitude: 
+    # longitude: 
+  }
+]
+locations.each do |location|
+    Location.create(
+      name: location[:name],
+      description: location[:description],
+      # user_id: locations.sample.user_id
+    )
+end
+puts "Locations successfully seeded"
